33dc30ca2c8bb99985ade6b1a1f1f0540d3b71a5
Morgane FRISBY <mfrisby@e2r10p7.42.fr>
2017-06-27 17:30:50 +0200

    file printf_c

--- libftprintf/ft_printf_c.c
+++ libftprintf/ft_printf_c.c
@@ -1,7 +1,18 @@
 #include "libftprintf.h"
 
-void    ft_printf_c()
+void    ft_printf_c(t_env *e)
 {
-    ft_putendl("ft_printf_c");
+    int c;
+    unsigned char uc;
+
+    uc = 0;
+    c = va_arg(e->pa, int);
+    if (c >= 0 && c <= 255)
+    {
+        uc = c;
+        e->result[e->index] = uc;
+        e->index++;
+        e->size++;
+    }
     return;
 }
