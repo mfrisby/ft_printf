5b9c089b23d65ee4dcd94e1045e7aaad1d701c9b
Morgane FRISBY <mfrisby@e2r8p5.42.fr>
2017-06-28 10:31:12 +0200

    add diese flag

--- libftprintf/ft_printf_x.c
+++ libftprintf/ft_printf_x.c
@@ -1,5 +1,35 @@
 #include "libftprintf.h"
 
+static void ft_printf_add_to_buffer(t_env *e, char *s, int start)
+{
+    if (e->precision > 0)
+    {
+        s[e->precision] = '\0';
+    }
+    while (e->index <= 3999 && s[start])
+    {
+        e->result[e->index] = s[start];
+        e->index++;
+        e->size++;
+        start++;
+    }
+    if (e->index >= 3999)
+    {
+        ft_printf_buffer_flush(e);
+        ft_printf_add_to_buffer(e, s, start);
+    }
+}
+
+
+static void     checkdieseflag(t_env *e, char *tmp)
+{
+    if (e->flag_diese == 1 && ft_strcmp("0", tmp) != 0 && 
+        ft_strcmp("-0", tmp) != 0 && ft_strcmp("+0", tmp) != 0)
+    {
+        ft_printf_add_to_buffer(e, "0x", 0);
+    }
+}
+
 void    ft_printf_x(t_env *e)
 {
     int i;
@@ -13,6 +43,8 @@ void    ft_printf_x(t_env *e)
         tmp[i] = ft_tolower(tmp[i]);
         i++;
     }
-    ft_putendl(tmp);
+    checkdieseflag(e, tmp);
+    ft_printf_add_to_buffer(e, tmp, 0);
+    //ft_putendl(tmp);
     return;
 }
