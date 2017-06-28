ae49e63196370c3b54484650e15d4a16da235b02
Morgane FRISBY <mfrisby@e2r8p5.42.fr>
2017-06-28 15:13:54 +0200

    fix precision

--- libftprintf/ft_printf_s.c
+++ libftprintf/ft_printf_s.c
@@ -2,7 +2,11 @@
 
 void    ft_printf_s(t_env *e)
 {
-    char *s = va_arg(e->pa, char*);
+    char *s;
+    
+    s = va_arg(e->pa, char*);
+    if (e->precision > 0 && (int)(ft_strlen(s))> e->precision)
+        s = ft_strsub(s, 0, e->precision);
     ft_printf_add_to_buffer(e, s, 0);
     return;
 }
