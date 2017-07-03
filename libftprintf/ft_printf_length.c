76808876ae95c2dcfffcd50c125d54a139ad302f
root <root@DESKTOP-CO7DB2A.localdomain>
2017-07-03 10:56:07 +0200

    flags hh ll l h j z turn on/off

--- libftprintf/ft_printf_length.c
+++ libftprintf/ft_printf_length.c
@@ -1,33 +1,46 @@
 #include "libftprintf.h"
 //hh h l ll j z
-int     ft_printf_length(t_env *e, char *format)
+static int  ft_printf_length2(t_env *e, char *format)
 {
     if (format[e->i + 1] && format[e->i] == 'h' && format[e->i + 1] != 'h')
     {
-        ft_putendl("h");
+        e->m_h = 1;
+        e->i += 1;
     }
     else if (format[e->i + 1] && format[e->i] == 'h' && format[e->i + 1] == 'h')
     {
-        ft_putendl("hh");
+        e->m_hh = 1;
+        e->i += 2;
     }
     else if (format[e->i + 1] && format[e->i] == 'l' && format[e->i + 1] != 'l')
     {
-        ft_putendl("l");
+        e->m_l = 1;
+        e->i += 1;
     }
-    else if (format[e->i + 1] && format[e->i] == 'l' && format[e->i + 1] == 'l')
+    else
+        return (0);
+    return (1);
+}
+int         ft_printf_length(t_env *e, char *format)
+{
+    if (ft_printf_length2(e, format) == 1)
+        return (0);
+    if (format[e->i + 1] && format[e->i] == 'l' && format[e->i + 1] == 'l')
     {
-        ft_putendl("ll");
+        e->m_ll = 1;
+        e->i += 2;
     }
     else if (format[e->i] == 'j')
     {
-        ft_putendl("j");
+        e->m_j = 1;
+        e->i += 1;
     }
     else if (format[e->i] == 'z')
     {
-        ft_putendl("z");
+        e->m_z = 1;
+        e->i += 1;
     }
     else
         return (-1);
-    e->i++;
     return (0);
 }
