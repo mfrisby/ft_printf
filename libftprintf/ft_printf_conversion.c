ae926848700fbf1add88f94504a461131f45fcb8
root <root@DESKTOP-CO7DB2A.localdomain>
2017-07-03 09:48:09 +0200

    min annule zero

--- libftprintf/ft_printf_conversion.c
+++ libftprintf/ft_printf_conversion.c
@@ -1,21 +1,8 @@
 #include "libftprintf.h"
 
-int        ft_printf_conversion(t_env *e, char c){
-    if (c == 'u')
-        ft_printf_u(e);
-    else if (c == 'U')
-        ft_printf_umaj(e);
-    else if (c == 's')
-        ft_printf_s(e);
-    else if (c == 'S')
-        ft_printf_smaj(e);
-    else if (c == 'p')
-        ft_printf_p(e);
-    else if (c == 'd' || c == 'i')
-        ft_printf_di(e);
-    else if (c == 'D')
-        ft_printf_dmaj(e);
-    else if (c == 'o')
+static int  ft_printf_conversion2(t_env *e, char c)
+{
+    if (c == 'o')
         ft_printf_o(e);
     else if (c == 'O')
         ft_printf_omaj(e);
@@ -27,6 +14,30 @@ int        ft_printf_conversion(t_env *e, char c){
         ft_printf_c(e);
     else if (c == 'C')
         ft_printf_cmaj(e);
+    else if (c == 'd' || c == 'i')
+        ft_printf_di(e);
+    else if (c == 'D')
+        ft_printf_dmaj(e);
+    else
+        return (-1);
+    return (1);
+}
+
+int        ft_printf_conversion(t_env *e, char c){
+    if (e->flag_moins == 1 && e->flag_zero == 1)
+        e->flag_zero = 0;//min annule zero
+    if (ft_printf_conversion2(e, c) == 1)
+        return (0);
+    if (c == 'u')
+        ft_printf_u(e);
+    else if (c == 'U')
+        ft_printf_umaj(e);
+    else if (c == 's')
+        ft_printf_s(e);
+    else if (c == 'S')
+        ft_printf_smaj(e);
+    else if (c == 'p')
+        ft_printf_p(e);
     else
         return (-1);
     return (0);
