5eaac6018dc4a5ebf2a5d33b0ca7f3d287e39b79
Morgane FRISBY <mfrisby@e1r11p16.42.fr>
2017-07-07 17:01:35 +0200

    smaj fix

--- libftprintf/ft_printf_smaj.c
+++ libftprintf/ft_printf_smaj.c
@@ -1,35 +1,38 @@
 #include "libftprintf.h"
 
-static void ft_printf_parse_smaj(t_env *e, wchar_t *s, int ret)
+static void ft_printf_parse_smaj(t_env *e, wchar_t *s)
 {
     char tmp[5];
+    int ret;
+    char *s1;
+
+    ret = 0;
+    s1 = ft_strnew(1);
     while (*s)
     {
         if (*s <= 127)
         {
             tmp[0] = (char)*s;
             tmp[1] = '\0';
-            ft_printf_add_to_buffer(e, tmp, 0);
         }
         else
         {
             ret = ft_printf_unicode(tmp, *s);
             tmp[ret] = '\0';
-            ft_printf_add_to_buffer(e, tmp, 0);
         }
+        s1 = ft_strjoin(s1, tmp);
         s++;
     }
+    ft_printf_putflags(e, s1);
 }
 
 void        ft_printf_smaj(t_env *e)
 {
-    int ret;
     wchar_t *s;
     
     s = va_arg(e->pa, wchar_t*);
-    ret = 0;
-    if (s == NULL)
-        ft_printf_add_to_buffer(e, "(null)", 0);
-    else
-        ft_printf_parse_smaj(e, s, ret);
+     if (s == NULL)
+         ft_printf_add_to_buffer(e, "(null)", 0);
+     else
+         ft_printf_parse_smaj(e, s);
 }
