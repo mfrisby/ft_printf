9638cf31c08453594a8775ff27a7348bcf3627f1
ysan-seb <ysan-seb@e1r9p8.42.fr>
2017-07-08 13:14:26 +0200

    FIX OMAJ

--- libftprintf/ft_printf_omaj.c
+++ libftprintf/ft_printf_omaj.c
@@ -4,7 +4,8 @@ void    ft_printf_omaj(t_env *e)
 {
     char *tmp;
 
+    e->type = 'O';
     unsigned long int octal = va_arg(e->pa, unsigned long int);
     tmp = ft_printf_itoabase_uli(octal, 8);
-    ft_printf_add_to_buffer(e, tmp, 0);
+    ft_printf_putflags(e, tmp);
 }
